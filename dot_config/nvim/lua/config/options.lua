6bab143dd3e7c647b92b5af3da6a0b81b5fa86bb
ettt2 <101014025+ettt2@users.noreply.github.com>
2024-08-12 16:13:56 +0900

    Update options.lua

--- dot_config/nvim/lua/config/options.lua
+++ dot_config/nvim/lua/config/options.lua
@@ -1,12 +1,17 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
--- vim.g.lazyvim_php_lsp = "intelephense"
 vim.g.autoformat = false
-vim.cmd([[
-  set noexpandtab
-]])
-local opt = vim.opt
-opt.expandtab = false
-opt.shiftwidth = 4
-opt.tabstop = 4
+vim.opt.scrolloff = 9999
+vim.opt.fileencoding = "utf-8"
+vim.opt.fileencodings = "utf-8,iso-2022-jp,sjis,euc-jp,cp932,utf-16"
+vim.opt.clipboard = "" -- WSLだと重いので無効化する
+vim.opt.mouse = ""
+vim.opt.wrapscan = true
+vim.opt.incsearch = true
+vim.opt.hlsearch = true
+vim.opt.shiftwidth = 4
+vim.opt.tabstop = 4
+vim.opt.softtabstop = 4
+vim.opt.showmatch = true
+vim.opt.spelllang = { "en", "cjk" }
