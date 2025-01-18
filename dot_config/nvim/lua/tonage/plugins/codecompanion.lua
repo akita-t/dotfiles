9ca2bb137cf6c28fc748c51e966397a7776de04d
tonage <101014025+tonage-t@users.noreply.github.com>
2025-01-18 23:42:22 +0900

    Update codecompanion.lua

--- dot_config/nvim/lua/tonage/plugins/codecompanion.lua
+++ dot_config/nvim/lua/tonage/plugins/codecompanion.lua
@@ -4,6 +4,9 @@ return {
     "nvim-lua/plenary.nvim",
     "nvim-treesitter/nvim-treesitter",
   },
+  keys = {
+    { "<leader>aa", "<cmd>CodeCompanionActions<cr>", desc = "Open CodeCompanion Action Palette" },
+  },
   config = function()
     require("codecompanion").setup({
       strategies = {
