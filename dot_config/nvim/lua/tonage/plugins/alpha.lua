da96168c77ea542353e6d30c40d34abf264e2219
ellllt <aknor1004@gmail.com>
2025-01-19 16:01:00 +0900

    Update alpha.lua

--- dot_config/nvim/lua/tonage/plugins/alpha.lua
+++ dot_config/nvim/lua/tonage/plugins/alpha.lua
@@ -18,7 +18,8 @@ return {
     -- Set menu
     dashboard.section.buttons.val = {
       dashboard.button("e", "  > New File", "<cmd>ene<CR>"),
-      dashboard.button("SPC ee", "  > Toggle file explorer", "<cmd>NvimTreeToggle<CR>"),
+      dashboard.button("-", "  > Open file explorer", "<CMD>Oil<CR>"),
+      dashboard.button("SPC -", "  > Open file explorer in floating window", "<CMD>Oil --float<CR>"),
       dashboard.button("SPC ff", "󰱼  > Find File", "<cmd>Telescope find_files<CR>"),
       dashboard.button("SPC fs", "  > Find Word", "<cmd>Telescope live_grep<CR>"),
       dashboard.button("SPC wr", "󰁯  > Restore Session For Current Directory", "<cmd>SessionRestore<CR>"),
