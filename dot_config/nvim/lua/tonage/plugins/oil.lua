08df20bf55aa659eda1a6cc0d0eff3e61aeebc14
tonage <101014025+tonage-t@users.noreply.github.com>
2025-01-19 13:39:17 +0900

    Update oil.lua

--- dot_config/nvim/lua/tonage/plugins/oil.lua
+++ dot_config/nvim/lua/tonage/plugins/oil.lua
@@ -3,6 +3,18 @@ return {
   ---@module 'oil'
   ---@type oil.SetupOpts
   opts = {},
-  -- Optional dependencies
-  dependencies = { { "nvim-tree/nvim-web-devicons", opts = {} } },
+  dependencies = { "nvim-tree/nvim-web-devicons" },
+  config = function ()
+    require("oil").setup({
+      view_options = {
+        show_hidden = true,
+      }
+    })
+
+    -- set keymaps
+    local keymap = vim.keymap
+
+    keymap.set("n", "-", "<CMD>Oil<CR>", { desc = "Open parent directory" })
+    keymap.set("n", "<leader>-", "<CMD>Oil --float<CR>", { desc = "Open parent directory in floating window" })
+  end
 }
