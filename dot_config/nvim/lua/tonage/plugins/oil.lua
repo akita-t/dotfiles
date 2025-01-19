8103f5460278ecf20c9310f345699e96b6194629
tonage <101014025+tonage-t@users.noreply.github.com>
2025-01-19 13:04:51 +0900

    Create oil.lua

---
+++ dot_config/nvim/lua/tonage/plugins/oil.lua
@@ -0,0 +1,8 @@
+return {
+  'stevearc/oil.nvim',
+  ---@module 'oil'
+  ---@type oil.SetupOpts
+  opts = {},
+  -- Optional dependencies
+  dependencies = { { "nvim-tree/nvim-web-devicons", opts = {} } },
+}
